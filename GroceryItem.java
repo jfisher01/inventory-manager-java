d455262b994b52b9b602815009c2bfd403b6c6cc
Johnfisher Uchem <103228361+jfisher01@users.noreply.github.com>
2024-06-14 02:10:22 -0500

    Update GroceryItem.java

--- GroceryItem.java
+++ GroceryItem.java
@@ -1,18 +1,15 @@
 package inventroyManager;
-
+import java.util.Scanner;
 /**
  * GroceryItem class extends Items class, implements Item Interface and represents grocery items
  * 
- * @author Johnfisher Uchem
- * @version 1.0 / 12/15/2023
- * @since Java Inventory Management System 1.0 
- *
  */
 public class GroceryItem extends Items implements Item 
 {
 	
+	Scanner keyboardInput = new Scanner(System.in);
 	/**
-	 * @param non-parameter constructor for this class
+	 * @param non-parameterized constructor for this grocery class
 	 */
 	public GroceryItem()
 	{
@@ -20,79 +17,77 @@ public class GroceryItem extends Items implements Item
 	}
 
 	/**
-	 * Parametric constructor for this class
-	 * @param id
-	 * @param name
-	 * @param quantity
-	 * @param price
-	 * @param category
+	 * Parameterized constructor for this grocery class
+	 * @param id the id of the grocery item
+	 * @param name the name of the grocery item
+	 * @param quantity the quantity of the grocery item
+	 * @param price the price of the grocery item
+	 * @param category the category of the grocery item
 	 */
 	public GroceryItem(String id, String name, int quantity, double price, String category) 
 	{
 		super(id, name, quantity, price, category);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 
 	/**
-	 * Sets id for the grocery item
-	 * @param id the id in the setId method's parameter 
+	 * @param id the id of the grocery item
 	 */
+	@Override
 	public void setId(String id) 
 	{
 		this.id = id;
 	}
 
 	/**
-	 * Sets name for the grocery item
-	 * @param name the name in the setName method's parameter
+	 * @param name the name of the grocery item
 	 */
+	@Override
 	public void setName(String name) 
 	{
 		this.name = name;
 	}
 
 	/**
-	 * Sets grocery item's quantity 
-	 * @param quantity the quantity in the setQuantity method's parameter
+	 * @param quantity the quantity of the grocery item
 	 */
+	@Override
 	public void setQuantity(int quantity) 
 	{
 		this.quantity = quantity;
 	}
 
 	/**
-	 * Sets grocery item's price
-	 * @param price the price in the setPrice method's parameter
+	 * @param price the price of the grocery item
 	 */
+	@Override
 	public void setPrice(double price) 
 	{
 		this.price = price;
 	}
 
 	/**
-	 * Sets grocery item's category
-	 * @param category the category in the setCategory method's parameter
+	 * @param category the category of the grocery item
 	 */
+	@Override
 	public void setCategory(String category) 
 	{
 		this.category = category;
 	}
 	
 	/**
-	 * Gets id for the grocery item
-	 * @return id, getId method
+	 * @return id the id of the grocery item
 	 */
 	@Override
 	public String getId() 
 	{
-		// TODO Auto-generated method stub
+		
 		return id;
 	}
 	
 	/**
-	 * Gets grocery item's name
-	 * @return name, getName method
+	 * @return the name of the grocery item
 	 */
 	@Override
 	public String getName() 
@@ -102,30 +97,26 @@ public class GroceryItem extends Items implements Item
 	}
 	
 	/**
-	 * Gets grocery items quantity
-	 * @return quantity, getQuantity method
+	 * @return the quantity of the grocery item
 	 */
 	@Override
 	public int getQuantity() 
 	{
-		// TODO Auto-generated method stub
+	
 		return quantity;
 	}
 	
 	/**
-	 * Gets grocery items price
-	 * @return price, getPrice method
+	 * @return the price of the grocery item
 	 */
 	@Override
 	public double getPrice() 
 	{
-		// TODO Auto-generated method stub
 		return price;
 	}
 	
 	/**
-	 * Gets grocery items category
-	 * @return category, getCategory method
+	 * @return category of the grocery item
 	 */
 	@Override
 	public String getCategory() 
@@ -135,15 +126,56 @@ public class GroceryItem extends Items implements Item
 	}
 	
 	/**
-	* Returns toString values of the constructor of GroceryItem class
-	 * @return values from toString method
+	 * 
+	 * @return the new grocery item object
+	 */
+	public GroceryItem enterGroceryItem() {
+		
+		System.out.println("We need the information for grocery items\n");	
+		
+		try {	
+				
+		  System.out.println("Enter grocery item's Id: "); 
+		  id = keyboardInput.nextLine();
+		  
+		  System.out.println("Enter grocery item's name: "); 
+		  name = keyboardInput.nextLine();
+		  
+		  System.out.println("Enter grocery item's quantity: "); 
+		  quantity = keyboardInput.nextInt();
+		  
+		  System.out.println("Enter grocery item's price: "); 
+		  price = keyboardInput.nextDouble();
+		  
+		  keyboardInput.nextLine();
+		  System.out.print("Enter grocery item's category: "); 
+		  category = keyboardInput.nextLine();
+		 
+		  new GroceryItem(id, name, quantity, price, category);
+
+		// Adds new item to inventory
+		InventoryManager.addItem(new GroceryItem(id, name, quantity, price, category));
+		
+	} 
+	catch (Exception e) 
+	{
+		// TODO Auto-generated catch block
+		System.out.println("Input mismatch!");
+		System.exit(0);
+	}
+	 return	new GroceryItem(id, name, quantity, price, category);
+	}
+	
+	
+	/**
+	 * @return the id, name, quantity, price and category of the grocery item
 	 */
 	@Override
 	public String toString() 
 	{
 			
 		return ("GROCERY ITEMS :\n" + "ID: " + id + "\nName: " + name + "\nQuantity: " + quantity + "\nPrice: " + fdollar.format(price)
-				+ "\nCategory: " + category + "\n");
+				+ "\nCategory: " + category + "\n\n");
 	}
 
 	
